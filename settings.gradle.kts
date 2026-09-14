9dbedb98faed3c2e1f0ed24aa56f11ae8b6745dc
Taxlot Dev <dev@taxlot.app>
2026-09-14 15:55:26 +0545

    fix(gradle): remove duplicate libs version catalog definition in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,11 +6,6 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    versionCatalogs {
-        create("libs") {
-            from(files("gradle/libs.versions.toml"))
-        }
-    }
     repositories {
         mavenCentral()
     }
